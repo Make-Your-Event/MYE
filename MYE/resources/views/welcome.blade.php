5d2b1cef384f9ee6d5565369b16a53ff8caf8ac9
Igor Rodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2022-12-19 16:27:41 -0300

    home page (incompleto acho)

--- MYE/resources/views/welcome.blade.php
+++ MYE/resources/views/welcome.blade.php
@@ -2,11 +2,30 @@
 @section('title','Make Your Event')
 @section('content')
 
-<h1>Crie seu Evento</h1>
+    <div id="search-container" class="col-md-12">
 
-    @foreach($events as $item)
+        <h1>Busque um evento</h1>
+        <form action="" class="input-group">
+            <input type="text" name="search" id="search" class="form-control" placeholder="Procurar evento">
+        </form>
 
-        <p>{{$item->nome}} -- {{$item->descricao}}</p>
-        
-    @endforeach
+    </div>
+
+    <div id="events-container" class="col-md-12">
+        <h2>Próximos eventos</h2>
+        <p class="subtitle">Veja os eventos dos próximos dias</p>
+        <div id="cards-container" class="row">
+            @foreach($events as $event)
+                <div class="card col-md-3">
+                    <img src="/img/event_2.jpg" alt="{{$event->nome}}">
+                    <div class="card-body">
+                        <p class="card-date">2022/10/09</p>
+                        <h5 class="card-title">{{$event->nome}}</h5>
+                        <p class="card-participants">X participantes</p>
+                        <a href="#" class="btn btn-primary">Saber mais</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
